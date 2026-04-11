67cdd7d942e926adebe34774c078f8a24ef9b150
skrofox <sangnguyendac825@gmail.com>
2026-04-11 15:56:30 +0700

    bug | fig bug while run cmd migrate

--- database/migrations/2026_04_06_063603_rename_academic_years_to_academic_batches_table.php
+++ database/migrations/2026_04_06_063603_rename_academic_years_to_academic_batches_table.php
@@ -4,15 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('academic_batches', function (Blueprint $table) {
-            //
+            // 1. Đổi tên bảng academic_years thành academic_batches
+            Schema::rename('academic_years', 'academic_batches');
+
+            // 2. Đổi tên cột trong bảng school_classes
+            Schema::table('school_classes', function (Blueprint $table) {
+                $table->renameColumn('academic_year_id', 'academic_batch_id');
+            });
         });
     }
 
@@ -21,8 +26,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('academic_batches', function (Blueprint $table) {
-            //
+        Schema::table('school_classes', function (Blueprint $table) {
+            $table->renameColumn('academic_batch_id', 'academic_year_id');
         });
+
+        Schema::rename('academic_batches', 'academic_years');
     }
 };
